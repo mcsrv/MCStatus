541b32b28fe47bcbef012c65f41013984cb86099
Hristijan <hgliguroski@gmail.com>
2023-02-26 14:39:58 +0100

    error fixes

--- src/routes/vote.js
+++ src/routes/vote.js
@@ -2,8 +2,14 @@ const { sendVote } = require("minecraft-server-util")
 
 module.exports=async function(req,res) {
     const {username, address, port, timestamp, secret} = req.body   
+    
+    if(!username) return res.writeHead(500).end(JSON.stringify({error: "Invalid username provided."}))
+    if(!address) return res.writeHead(500).end(JSON.stringify({error: "Invalid address provided."}))
+    if(!port || isNaN(parseInt(port))) return res.writeHead(500).end(JSON.stringify({error: "Invalid port provided."}))
+    if(!secret) return res.writeHead(500).end(JSON.stringify({error: "Invalid secret provided."}))
 
-    await sendVote(address, port, {
+
+    await sendVote(address, parseInt(port), {
         username,
         timestamp: timestamp ?? Date.now(),
         token: secret,
